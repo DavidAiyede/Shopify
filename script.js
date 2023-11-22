be7889e097f717189f61ac552c8bc841f4e0c332
DavidAiyede <davidaiyede27@gmail.com>
2023-11-22 20:13:28 +0100

    newpush

---
+++ script.js
@@ -0,0 +1,8 @@
+function davii(){
+    const tog = document.querySelector(".DC")
+    tog.classList.toggle("popUp")
+}
+function notification(){
+    const togg = document.querySelector(".notify")
+    togg.classList.toggle("pop")
+}
